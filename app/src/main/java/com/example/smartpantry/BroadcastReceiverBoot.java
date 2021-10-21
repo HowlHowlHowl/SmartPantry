4168ff69e461b6b6ba334944f4f1643b9f453922
HowlHowlHowl <Limoni99>
2021-10-21 20:53:59 +0200

    USLESS RESOURCEs REMOVED

--- app/src/main/java/com/example/smartpantry/BroadcastReceiverBoot.java
+++ app/src/main/java/com/example/smartpantry/BroadcastReceiverBoot.java
@@ -3,17 +3,13 @@ package com.example.smartpantry;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
 
 public class BroadcastReceiverBoot extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        //TODO TEST
-        Log.println(Log.ASSERT, "BC RECEIVER", "INTENT RECEIVED");
+        //Due to a recent update this only works if the app has been opened at least one time
+        //if the app get force-closed by the user it has to be opened again
         if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            Toast.makeText(context, "Broadcast Started", Toast.LENGTH_LONG).show();
-            Log.println(Log.ASSERT, "BC RECEIVER", "INTENT RECEIVED, IF");
             Intent intentServiceSetAlarm = new Intent(context, IntentServiceSetAlarm.class);
             context.startService(intentServiceSetAlarm);
         }
